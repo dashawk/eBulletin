efd5bf18fb5c05fb80007ac6b251c9b6ecc4c104
dashawk <magnum_mack5@yahoo.com>
2012-02-20 14:30:28 -0800

    updated request.php

--- api/request.php
+++ api/request.php
@@ -5,14 +5,3 @@
 	$e = new eAPI;
 	
 	$e->request();
-	//$e->ValidateUser('test','test');
-	
-	//if($e->doLogin->error == 0){
-	//	echo $e->doLogin->message;
-	//} else {
-	//	echo $e->doLogin->message;
-	//}
-	//$s = $e->eBulletin->Query('SELECT * FROM tbl_users');
-	
-	//echo '<pre>';
-	//var_dump($s);
